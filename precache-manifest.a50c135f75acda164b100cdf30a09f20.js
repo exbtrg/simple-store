8ea251306fcca912b8dadf8548acedef334d335b
exbtrg <exbtrg@gmail.com>
2020-06-21 20:31:43 +0300

    Updates

---
+++ precache-manifest.a50c135f75acda164b100cdf30a09f20.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8779de77845211a55810f7783c9a4bf",
+    "url": "/simple-store/index.html"
+  },
+  {
+    "revision": "edd42c95071e067763a2",
+    "url": "/simple-store/static/css/main.52a1dbd4.chunk.css"
+  },
+  {
+    "revision": "1fae46cb7aa39784cfc0",
+    "url": "/simple-store/static/js/2.2276b036.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/simple-store/static/js/2.2276b036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd42c95071e067763a2",
+    "url": "/simple-store/static/js/main.68fe67c8.chunk.js"
+  },
+  {
+    "revision": "0aeb83081a15d70ca52f",
+    "url": "/simple-store/static/js/runtime-main.3950ab9d.js"
+  },
+  {
+    "revision": "9324e856c8d8523d6358e8108ab3321d",
+    "url": "/simple-store/static/media/404-error.9324e856.svg"
+  },
+  {
+    "revision": "9eecd03232ae736def5587ccaa6a5fb1",
+    "url": "/simple-store/static/media/cart.9eecd032.svg"
+  },
+  {
+    "revision": "3e0404636a02835253c01d49632e5aee",
+    "url": "/simple-store/static/media/minus.3e040463.svg"
+  },
+  {
+    "revision": "99ff40a331ff2d80691d05d918fd6874",
+    "url": "/simple-store/static/media/plus.99ff40a3.svg"
+  },
+  {
+    "revision": "9ba7cb3979f3529d75bac6e44f89b4ed",
+    "url": "/simple-store/static/media/trash.9ba7cb39.svg"
+  }
+]);
